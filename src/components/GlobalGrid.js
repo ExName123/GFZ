f7f058ebe9f6d745a66127e12a56f045ea19e3a8
ExName123 <exnew88@mail.ru>
2025-04-16 15:49:26 +0300

    Init

---
+++ src/components/GlobalGrid.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const GlobalGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  width: 100%;
+`;
+
+export default GlobalGrid;
