512877b20f1d644026218b3dabe302f61ca0d631
ExName123 <exnew88@mail.ru>
2025-04-16 14:16:41 +0300

    Updates

--- src/components/GlobalGrid.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const GlobalGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  width: 100%;
-`;
-
-export default GlobalGrid;
